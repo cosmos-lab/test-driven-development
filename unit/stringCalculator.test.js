f9876bfc25c7c08ddf4594f64c87e23e2d22e9f9
PD <cosmos.lab.contact@gmail.com>
2024-09-01 20:51:33 +0530

    [Unit] Add test for handling new line characters between numbers

--- unit/stringCalculator.test.js
+++ unit/stringCalculator.test.js
@@ -14,4 +14,10 @@ describe("String Calculator", () => {
     expect(add("1,5")).toBe(6);
     expect(add("2,3,5")).toBe(10);
   });
+
+  it("should handle new lines between numbers", () => {
+    expect(add("1\n2,3")).toBe(6);
+    expect(add("4\n5\n\n6")).toBe(15);
+    expect(add("\n4\n5,6\n\n7")).toBe(22);
+  });
 });
