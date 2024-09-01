d8ad4f0a2377211aaedc7367d1a0517c0e136f38
PD <cosmos.lab.contact@gmail.com>
2024-09-01 20:46:48 +0530

    [Src] Handle multiple comma-separated numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,6 +1,9 @@
 function add(numbers) {
   if (!numbers) return 0;
-  return Number(numbers);
+  return numbers
+    .split(",")
+    .map(Number)
+    .reduce((a, b) => a + b, 0);
 }
 
 export { add };
