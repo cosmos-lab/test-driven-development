0a4cfccd83ede3f191617298b9ad8fb2f2dd71bb
PD <cosmos.lab.contact@gmail.com>
2024-09-01 22:26:49 +0530

    [Src] Handle custom delimiters between numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,10 +1,14 @@
 function add(numbers) {
   if (!numbers) return 0;
   let delimiter = /[\n,]/;
-  return numbers
-    .split(delimiter)
-    .map(Number)
-    .reduce((a, b) => a + b, 0);
+  if (numbers.startsWith("//")) {
+    const delimiterLineIndex = numbers.indexOf("\n");
+    delimiter = new RegExp(numbers.substring(2, delimiterLineIndex), "g");
+    numbers = numbers.substring(delimiterLineIndex + 1);
+  }
+
+  const numberArray = numbers.split(delimiter).filter(Boolean).map(Number);
+  return numberArray.reduce((a, b) => a + b, 0);
 }
 
 export { add };
