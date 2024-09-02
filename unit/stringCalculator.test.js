f1ec6f883be9cf987f1df824cdc868556f8d2111
PD <cosmos.lab.contact@gmail.com>
2024-09-02 21:22:23 +0530

    [Refactor][Unit] Combine tests for commas and new line characters

--- unit/stringCalculator.test.js
+++ unit/stringCalculator.test.js
@@ -10,12 +10,9 @@ describe("String Calculator", () => {
     expect(add("5")).toBe(5);
   });
 
-  it("should return the sum of multiple numbers separated by commas", () => {
+  it("should return the sum of multiple numbers separated by commas or new line characters", () => {
     expect(add("1,5")).toBe(6);
     expect(add("2,3,5")).toBe(10);
-  });
-
-  it("should handle new lines between numbers", () => {
     expect(add("1\n2,3")).toBe(6);
     expect(add("4\n5\n\n6")).toBe(15);
     expect(add("\n4\n5,6\n\n7")).toBe(22);
